7e8f9170c72caca2473f80c52d419e1283154989
Arpan Patel <49106359+adp8ke@users.noreply.github.com>
2022-09-16 12:26:26 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+airflow db init
+
+mkdir ~/airflow/dags
+
+mv /workspace/example-airflow-dataproc-astra/airflow/poc.py ~/airflow/dags
+
+airflow users create \
+    --username admin \
+    --password admin \
+    --firstname Peter \
+    --lastname Parker \
+    --role Admin \
+    --email spiderman@superhero.org
+
+airflow variables import /workspace/example-airflow-dataproc-astra/variables.json
+
+airflow webserver --port 8080 &
+
+airflow scheduler &
