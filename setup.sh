d46f4661b76941cab9fb1701df71b509cf64144f
Arpan Patel <49106359+adp8ke@users.noreply.github.com>
2022-09-16 12:45:27 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ airflow db init
 
 mkdir ~/airflow/dags
 
-mv /workspace/example-airflow-dataproc-astra/airflow/poc.py ~/airflow/dags
+mv /workspace/example-airflow-dataproc-astra/poc.py ~/airflow/dags
 
 airflow users create \
     --username admin \
